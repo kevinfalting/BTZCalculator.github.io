0075aaff70494e6e943f8f765694a2df66a4fc3c
Kevin Falting <kevinfalting@gmail.com>
2017-09-28 10:12:54 -0500

    finished btz calculations. Ready for testing

--- assets/calculator.js
+++ assets/calculator.js
@@ -8,31 +8,34 @@ document.addEventListener('DOMContentLoaded', function() {
 function calculate() {
 
     // gather all of the important elements on the page
-    const years = document.getElementById("years").value;
-    const enlistmentDate = document.getElementById("enlistmentDate").value;
-    const a1cDate = document.getElementById("a1cDate").value;
+    var enlistmentDate = document.getElementById("enlistmentDate").value;
+    var a1cDate = document.getElementById("a1cDate").value;
 
     var btzBoardDate = document.getElementById("btzBoardDate");
     var sraBTZSewOnDate = document.getElementById("sraBTZSewOnDate");
     var sraSewOnDate = document.getElementById("sraSewOnDate");
 
-    if (years == 6) {
-        btzBoardDate.innerHTML = calculateBoardDate(moment(a1cDate));
-        sraBTZSewOnDate.innerHTML = moment(a1cDate).add(22, "M").format("MMMM Do, YYYY");
-        sraSewOnDate.innerHTML = moment(a1cDate).add(28, "M").format("MMMM Do, YYYY");
-    } else if (years == 4) {
-        // TODO: add logic for calculating a 4 year enlistee
-        alert("Oops! Haven't coded this one yet. Standby!")
-    } else {
-        console.error("There was a selection other than 4 or 6 on the number of years enlisted.")
+    // calculate tis (Time In Service) and tig (Time In Grade for A1C) eligibility dates
+    var tis36 = moment(enlistmentDate).add(36, "M");
+    var tig20 = moment(a1cDate).add(20, "M");
+    var tig28 = moment(a1cDate).add(28, "M");
+
+    if (tis36 >= tig28) {
+        sraSewOnDate.innerHTML = tig28.format("MMMM Do, YYYY");
+        sraBTZSewOnDate.innerHTML = tig28.subtract(6, "M").format("MMMM Do, YYYY");
+        btzBoardDate.innerHTML = calculateBoardDate(tig28);
+    } else if (tis36 <= tig20) {
+        sraSewOnDate.innerHTML = tig20.format("MMMM Do, YYYY");
+        sraBTZSewOnDate.innerHTML = tig20.subtract(6, "M").format("MMMM Do, YYYY");
+        btzBoardDate.innerHTML = calculateBoardDate(tig20);
     }
+
 }
 
 // returns the month of the quarter prior to the month they would sew on BTZ
-function calculateBoardDate(a1cDate) {
-    a1cDate = a1cDate.add(22, "M");
-    var quarter = a1cDate.quarter() - 1;
-    var year = a1cDate.year();
+function calculateBoardDate(btzDate) {
+    var year = btzDate.year();
+    var quarter = btzDate.quarter() - 1; // select the quarter previous to the quarter you would sew on SrA
 
     switch (quarter) {
         case 0:
